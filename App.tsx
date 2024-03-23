a6570811782e182db7f0fabd4a8883b381a1fd51
DouglasFJ <douglasfj2004@gmail.com>
2024-03-23 12:23:35 -0300

    feat: banco de dados Adicionando comunicao com o banco de dados function para iniciar o banco de forma padrao

--- App.tsx
+++ App.tsx
@@ -1,26 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { dispositivoPossuiAutenticaoLocal, requerirAutenticacao } from './src/services/auth/AuthService';
-import { useState } from 'react';
-import * as LocalAuth from "expo-local-authentication";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Asset, useAssets } from 'expo-asset';
+import * as FileSystem from 'expo-file-system';
+import { InitParams } from './src/model/initParamsModel';
 
 export default function App() {
-  const [isAuth, setIsAuth] = useState<boolean>(false)
 
-  console.debug(isAuth)
-  if(!isAuth){
-    LocalAuth.authenticateAsync({
-      promptMessage: "Autenticar para acessar o app",
-    }).then(resultAuth=>{
-      setIsAuth(resultAuth.success)
-    })
-  }
+  onInit({})
 
   return (
     <View style={styles.container}>
       <Text>Meu primeiro app mobile com REACT NATIVE</Text>
-      <StatusBar style="auto" />
-      <Text>Está autenticado: {isAuth? "true": "false"}</Text>
     </View>
   );
 }
@@ -33,3 +22,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function onInit(params: InitParams) {
+  database(params)
+
+}
+
+function database(params: InitParams) {
+  // verificar se o banco de dados já existe e criar caso não exista
+}
--- src/services/db/SqLiteService.ts
+++ src/services/db/SqLiteService.ts
@@ -1,8 +1,12 @@
 import * as SQLite from "expo-sqlite";
 import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
 
 const PATH_SQLITE_FILES = `${FileSystem.documentDirectory}'SQLite'`
-export const DATABASE_NAME = "MyFinances"
+export const MYFINANCES_FILE_DATABASE_NAME = "MyFinances"
+export const MYFINANCES_DATABASE_NAME = "MY_FINANCES_DATABASE"
+const LOCAL_DDL_FILE = "./assets/db/ddl-myfinances.sql"
+const LOCAL_DML_FILE = "./assets/db/dml-myfinances.sql"
 
 /**
  * Método para abrir/criar um banco SQLite
@@ -14,16 +18,63 @@ export const DATABASE_NAME = "MyFinances"
  * 
  * @returns objeto de manipulação do banco de dados
  */
-async function openDatabase(name: string) {
+export async function openDatabase(name: string) {
     name += ".db"
 
     if (!(await FileSystem.getInfoAsync(PATH_SQLITE_FILES)).exists) {
         await FileSystem.makeDirectoryAsync(PATH_SQLITE_FILES);
     }
-
+    console.debug(`Opening database in path: ${FileSystem.documentDirectory}SQLite/${name}`)
     return SQLite.openDatabase(name)
 }
 
 export async function select() {
-    let db = await openDatabase(DATABASE_NAME)
+    let db = await openDatabase(MYFINANCES_FILE_DATABASE_NAME)
+
+}
+
+/**
+ * Essa função inicializa o banco de dados e todo o schema de forma padrão do sistema
+ */
+export async function initDefaultDatabase() {
+    let sqlDDL: string = "";
+    let sqlDML: string = "";
+
+    //lendo os arquivos com o SQL
+    try {
+        const ddlAsset = Asset.fromModule(require(LOCAL_DDL_FILE))
+        const dmlAsset = Asset.fromModule(require(LOCAL_DML_FILE))
+
+        await ddlAsset.downloadAsync()
+        await dmlAsset.downloadAsync()
+
+        sqlDDL = ddlAsset.localUri ? await FileSystem.readAsStringAsync(ddlAsset.localUri) : ""
+        sqlDML = dmlAsset.localUri ? await FileSystem.readAsStringAsync(dmlAsset.localUri) : ""
+        
+    } catch (error) {
+        console.error("Erro ao ler o arquivo sql: " + error)
+    }
+
+    // executando SQLs
+    let db = await openDatabase(MYFINANCES_FILE_DATABASE_NAME)
+    db.transaction((t) => {
+
+        t.executeSql(sqlDDL, [],
+            (tr, r) => {
+                console.debug("DDL executado com sucesso: "+r)
+
+                tr.executeSql(sqlDML, [],
+                    (trx, rs)=>{
+                        console.debug("DML executado com sucesso: " + rs)
+                    }
+                ) 
+            },
+            (trx, error) => {
+                console.error("Erro ao executar o DDL: "+error)
+                return false;
+            }
+        )
+    }, (error)=>{
+        console.error("Erro ao iniciar uma transação SQL: "+error)
+    })
 }
